ef3184c1d2a2b89cd2c1992b4209d2c3ab6825c8
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2020-10-07 14:43:20 -0300

    GrpcDemo

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            // var input = new HelloRequest {Name = "Lucas"};
+            // var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            // var client = new Greeter.GreeterClient(channel);
+            //
+            // var reply = await client.SayHelloAsync(input);
+            //
+            // Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var request = new CustomerLookupModel{ UserId = 1 };
+
+            var customer = await customerClient.GetCustomerInfoAsync(request);
+
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+            Console.WriteLine();
+            Console.WriteLine("New Customers List");
+            Console.WriteLine();
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName}: {currentCustomer.EmailAddress}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            switch (request.UserId)
+            {
+                case 1:
+                    output.FirstName = "Fulano";
+                    output.LastName = "da Silva";
+                    break;
+                case 3:
+                    output.FirstName = "Beltrano";
+                    output.LastName = "Santos";
+                    break;
+                default:
+                    output.FirstName = "Lucas";
+                    output.LastName = "Souza";
+                    break;
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            var customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Lucas",
+                    LastName = "Souza",
+                    Age = 30,
+                    EmailAddress = "lfrigodesouza@outlook.com",
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Sue",
+                    LastName = "Storm",
+                    EmailAddress = "sue@storm.com",
+                    Age = 28,
+                    IsAlive = false
+                },
+                new CustomerModel
+                {
+                    FirstName = "Bilbo",
+                    LastName = "Baggings",
+                    EmailAddress = "bilbo@middleearth.net",
+                    Age = 117,
+                    IsAlive = false
+                }
+            };
+
+            foreach (var customer in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
